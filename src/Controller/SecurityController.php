7e3e32fc6d07595b0092c378bd4b788289de7b5a
ChristianCorciu <cristicorciu2@gmail.com>
2025-07-06 17:56:53 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,3 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
 use GuzzleHttp\Client;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
