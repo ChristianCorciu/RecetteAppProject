79ee927560de741a21df94978056507d321fa5d4
ChristianCorciu <cristicorciu2@gmail.com>
2025-07-06 18:14:54 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,51 +4,29 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use GuzzleHttp\Client;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-       if ($request->isMethod('POST')) {
-        $postData = $request->request->all();
-        $logger->info('Données POST reçues', $postData);
-            $client = new Client();
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
 
-            try {
-                $response = $client->post('https://pjrvjsjzoucvboyluonr.supabase.co/auth/v1/token', [
-                    'headers' => [
-                        'apikey' => 'VOTRE_SUPABASE_API_KEY',
-                        'Content-Type' => 'application/json',
-                    ],
-                    'json' => [
-                        'email' => $email,
-                        'password' => $password,
-                    ],
-                ]);
-
-                $data = json_decode($response->getBody(), true);
-
-                if (isset($data['access_token'])) {
-                    $this->get('session')->set('user_token', $data['access_token']);
-
-                    return $this->redirectToRoute('homepage');
-                } else {
-                    $error = 'Échec de la connexion';
-                }
-            } catch (\Exception $e) {
-                $error = 'Erreur lors de la connexion : ' . $e->getMessage();
-            }
-        } else {
-            $error = $authenticationUtils->getLastAuthenticationError();
-        }
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
-            'error' => $error ?? null,
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
